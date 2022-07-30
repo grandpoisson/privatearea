05199860112ed461dd7c11f8d451309e1734642e
grandpoisson <grandpoisson@gmail.com>
2022-07-30 14:55:36 +0300

    display contents of markdown file in private area if is subscriber

--- pages/members.js
+++ pages/members.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { getMarkdown } from "lib/markdown";
 
-export default function Members() {
+export default function Members({ markdown }) {
   const router = useRouter();
 
   const { data: session, status } = useSession();
@@ -34,16 +35,21 @@ export default function Members() {
       <div className="text-center ">
         <h1 className=" mt-20 font-extrabold text-2xl">Private Area</h1>
 
-        <p className="mt-10">Thank you for being a member</p>
-
-        <p className="mt-10">You now have access to:</p>
-
-        <ol className="mt-10 list-inside list-decimal">
-          <li>The lyrics book in PDF</li>
-          <li>Exclusive 30% discount on the albums</li>
-          <li>Exclusive access to preorders</li>
-        </ol>
+        <div
+          className="markdown"
+          dangerouslySetInnerHTML={{ __html: markdown }}
+        />
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const markdown = await getMarkdown();
+
+  return {
+    props: {
+      markdown,
+    },
+  };
 }
